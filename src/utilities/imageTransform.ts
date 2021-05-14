15b38dce936cdfc0cc7512df3260e3e93aa6d9e2
Alfonsina Lizardo <alfonsina.l@gmail.com>
2021-05-14 18:42:27 -0400

    fix: create folder for the output image if it doesn't exist

--- src/utilities/imageTransform.ts
+++ src/utilities/imageTransform.ts
@@ -1,5 +1,7 @@
 import path from 'path';
 import sharp from 'sharp';
+import { promises as fsPromises } from 'fs';
+import fs from 'fs';
 
 // Resizes the image to the width and height set in the parameters
 const imageTransform = async (
@@ -9,10 +11,15 @@ const imageTransform = async (
 ): Promise<string> => {
   const imageInput =
     path.join(__dirname, '../', 'assets/', 'originals/', filename) + '.jpg';
+  const imageOutputFolder = path.join(__dirname, '../', 'assets/', 'thumbnails/');
   const imageOutput =
     path.join(__dirname, '../', 'assets/', 'thumbnails/', filename) +
     `-${width}-${height}.jpg`;
-
+  
+  if (!fs.existsSync(imageOutputFolder)) {
+    await fsPromises.mkdir(imageOutputFolder);
+  }
+  
   try {
     // await for sharp to process the image, if it succeds, returns the imageOutput
     await sharp(imageInput).resize(width, height).toFile(imageOutput);
